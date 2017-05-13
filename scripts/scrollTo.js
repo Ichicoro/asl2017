2fa2758ed5a733f1e33b5f988e0984c3e90027df
Ichicoro <ichicoro@users.noreply.github.com>
2017-05-13 21:08:55 +0200

    Fixed the font

--- scripts/scrollTo.js
+++ scripts/scrollTo.js
@@ -1,24 +1,16 @@
 $(document).on('scroll', () => {
-	//document.getElementById("header").className = "transparent";
-
-	/* $('.navbarlinkdark').each((i) => {
-		$(this).removeClass("navbarlinkdark").addClass("navbarlinklight");
-	})
-	$('.navbar_divider').attr("color", "#f2f2f2"); */
-
 	$(".colored").each((i, e) => {
-		//console.log(i + " " + e);
 		$(e).removeClass("darktext").addClass("lighttext");
 
 		if ($(document).scrollTop() + parseInt(51) >=$('#about').position().top) {
 			$(e).removeClass("lighttext").addClass("darktext");
 		}
 		
-		if ($(document).scrollTop() - parseInt(51) >=$('#projects').position().top){
+		if ($(document).scrollTop() - parseInt(51) >=$('#projects').position().top) {
 			$(e).removeClass("darktext").addClass("lighttext");
 		}
 
-		if($(document).scrollTop() + parseInt(51) >=$('#contact').position().top){
+		if($(document).scrollTop() + parseInt(51) >=$('#contact').position().top) {
 			$(e).removeClass("lighttext").addClass("darktext");
 		}
 	})
