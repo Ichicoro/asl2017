06a4630bbef540d100f6c246bd40d0758749c6c8
Ichicoro <ichicoro@users.noreply.github.com>
2017-05-14 15:01:09 +0200

    Some lorem ipsum stuff

--- scripts/scrollTo.js
+++ scripts/scrollTo.js
@@ -2,7 +2,11 @@ $(document).on('scroll', () => {
 	$(".colored").each((i, e) => {
 		$(e).removeClass("darktext").addClass("lighttext");
 
-		if ($(document).scrollTop() + parseInt(51) >=$('#about').position().top) {
+		if ($(document).scrollTop() + parseInt(51) >=$('#opinioncontent').position().top) {
+			$(e).removeClass("lighttext").addClass("darktext");
+		}
+
+		if ($(document).scrollTop() + parseInt(40) >=$('#about').position().top) {
 			$(e).removeClass("lighttext").addClass("darktext");
 		}
 		
