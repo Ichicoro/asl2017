767dc31b4e29dbf0004fdce20de3d30037a64948
Ichicoro <ichicoro@users.noreply.github.com>
2017-05-15 10:25:44 +0200

    Farewell, slanted divs :(

--- scripts/scrollTo.js
+++ scripts/scrollTo.js
@@ -1,21 +1,35 @@
 $(document).on('scroll', () => {
 	$(".colored").each((i, e) => {
-		$(e).removeClass("darktext").addClass("lighttext");
+		toWhite(e);
 
-		if ($(document).scrollTop() + parseInt(51) >=$('#opinioncontent').position().top) {
-			$(e).removeClass("lighttext").addClass("darktext");
+		if ($(document).scrollTop() + parseInt(25) >=$('#opinioncontent').position().top) {
+			toDark(e);
 		}
 
-		if ($(document).scrollTop() + parseInt(40) >=$('#about').position().top) {
-			$(e).removeClass("lighttext").addClass("darktext");
+		if ($(document).scrollTop() + parseInt(25) >=$('#about').position().top) {
+			toDark(e);
 		}
 		
-		if ($(document).scrollTop() - parseInt(25) >=$('#projects').position().top) {
-			$(e).removeClass("darktext").addClass("lighttext");
+		if ($(document).scrollTop() + parseInt(25) >=$('#projects').position().top) {
+			toWhite(e);
 		}
 
-		if($(document).scrollTop() + parseInt(40) >=$('#contact').position().top) {
-			$(e).removeClass("lighttext").addClass("darktext");
+		if($(document).scrollTop() + parseInt(25) >=$('#contact').position().top) {
+			toWhite(e);
+		}
+
+		if($(document).scrollTop() + parseInt(25) >=$('#timetablecontent').position().top) {
+			toDark(e);
 		}
 	})
 })
+
+var toWhite = (e) => {
+	$(e).removeClass("darktext").addClass("lighttext");
+	$("#header").removeClass("whitebg").addClass("transparent");
+}
+
+var toDark = (e) => {
+	$(e).removeClass("lighttext").addClass("darktext");
+	$("#header").removeClass("transparent").addClass("whitebg");
+}
--- scripts/toggleslanted.js
+++ scripts/toggleslanted.js
@@ -1,13 +1,11 @@
 $("#slantedtoggle").click(() => {
     if (slanted) {
         $(".titlediv").each((i, e) => {
-            console.log(e)
-            $(e).removeClass("clipped");
+            $(e).removeClass("clipped").removeClass("clippedtop");
         })
         slanted = false;
     } else {
         $(".titlediv").each((i, e) => {
-            console.log(e)
             $(e).addClass("clipped");
         })
         slanted = true;
